bca50ea71f2743ba242a557791907c8011437611
Federico Macías <fma.abogado@gmail.com>
2022-07-13 16:56:19 -0500

    Created the Workout component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,33 @@
+import {useEffect,useState} from 'react'
+
+//components
+import WorkoutDetails from '../components/WorkoutDetails';
+
 const Home = () => {
+  const [workouts,setWorkouts] = useState(null)
+  useEffect(() => {
+    //Se crea una función para el fetch porque se 
+    //require una funcion asincrona y no se puede usar el async
+    //directamente en el useEffect
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts');
+      const json = await response.json();
+      if(response.ok){
+        setWorkouts(json);
+
+      }
+    }
+    fetchWorkouts();
+  }, []);
   return (
     <div className='home'>
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts && workouts.map((workout)=>(
+          <WorkoutDetails key={workout._id} workout={workout}/>
+        )
+
+        )}
+      </div>
     </div>
   )
 }
